4f552265c7cff8414ea1d94f54bfd2fdf20d78d3
koba-masa <kobayashi_mshr@koba-masa.com>
2023-01-14 21:55:43 +0900

    feat: スクレイピングするための親クラスを実装

---
+++ src/app/models/web_page.rb
@@ -0,0 +1,16 @@
+class WebPage
+  def user_agent
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+  end
+
+  def url
+    raise NotImplementedError
+  end
+
+  def sleep
+    sleep(5)
+  end
+
+  def get
+  end
+end
