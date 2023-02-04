fd0ffab312f50ffd2878d0a873af60529f9427d3
koba-masa <kobayashi_mshr@koba-masa.com>
2023-02-04 23:18:55 +0900

    feat: Seleniumを使用してgetする処理を追加

--- src/app/models/web_page.rb
+++ src/app/models/web_page.rb
@@ -1,19 +1,45 @@
-require 'open-uri'
-
 class WebPage
   def user_agent
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-  end
-
-  def url
-    raise NotImplementedError
   end
 
   def sleep
     sleep(5)
   end
 
-  def get
-    URI.open(url, 'User-Agent' => user_agent)
+  def get(url)
+    driver.navigate.to url
+  end
+
+  def driver
+    return @driver if @driver.present?
+    unless Rails.env.produciton?
+      client = Selenium::WebDriver::Remote::Http::Default.new
+      client.read_timeout = 180
+      @driver = Selenium::WebDriver.for(
+        :remote,
+        :url => 'http://selenium_server:4444',
+        desired_capabilities: :chrome,
+        http_client: client
+      )
+    end
+    @driver.manage.timeouts.implicit_wait = 10
+    @driver
+  end
+
+  def options
+    return @options if @options.present?
+    @options = Selenium::WebDriver::Chrome::Options.new
+    @options.add_argument('--headless')
+
+    @options
+  end
+
+  def close
+    driver.close
+  end
+
+  def quit
+    driver.quit
   end
 end
--- src/spec/models/web_pages/loto6_spec.rb
+++ src/spec/models/web_pages/loto6_spec.rb
@@ -4,11 +4,14 @@ module WebPages
   RSpec.describe Loto6, type: :model do
     let(:instance) { described_class.new }
     describe 'get' do
+      after do
+        instance.close
+        instance.quit
+      end
+
       it 'ロト6のページの内容が取得できること' do
-        response = instance.get
-        binding.pry
-        expect(response.status).to contain_exactly('200', 'OK')
-        expect(response.read).not_to be nil
+        instance.get(instance.url)
+        expect(instance.driver.page_source).not_to eq nil
       end
     end
   end
