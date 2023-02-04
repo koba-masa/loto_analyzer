f1b8f6658aa90993e336b6c34b92bb1a24eba99f
koba-masa <kobayashi_mshr@koba-masa.com>
2023-02-05 00:24:40 +0900

    feat: 開催回を取得する処理を追加

--- src/app/models/web_pages/loto6.rb
+++ src/app/models/web_pages/loto6.rb
@@ -2,4 +2,12 @@ class WebPages::Loto6 < WebPage
   def url
     'https://www.mizuhobank.co.jp/retail/takarakuji/check/loto/loto6/index.html'
   end
+
+  def times
+    times = driver.find_element(:css, 'table.typeTK thead tr th:nth-child(2)').text
+    m = Regexp.new('第([0-9]{1,})回').match(times)
+    raise NoMatchingPatternError if m.nil? || m[1].nil?
+
+    m[1]
+  end
 end
--- src/spec/models/web_pages/loto6_spec.rb
+++ src/spec/models/web_pages/loto6_spec.rb
@@ -3,15 +3,26 @@ require 'rails_helper'
 module WebPages
   RSpec.describe Loto6, type: :model do
     let(:instance) { described_class.new }
-    describe 'get' do
-      after do
-        instance.close
-        instance.quit
-      end
+    let(:url) { 'http://web:80/loto6/index.html' }
 
+    before do
+      instance.get(url)
+    end
+
+    after do
+      instance.close
+      instance.quit
+    end
+
+    describe 'get' do
       it 'ロト6のページの内容が取得できること' do
-        instance.get(instance.url)
         expect(instance.driver.page_source).not_to eq nil
+      end
+    end
+
+    describe '#times' do
+      it '開催回を返却すること' do
+        expect(instance.times).to eq '1763'
       end
     end
   end
