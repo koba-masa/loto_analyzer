10ff12c543048c65e12d5513fb0306561bf6ede9
koba-masa <kobayashi_mshr@koba-masa.com>
2023-02-05 16:07:35 +0900

    feat: 抽選日、本数字、ボーナス数字を取得する処理を追加

--- src/app/models/web_pages/loto6.rb
+++ src/app/models/web_pages/loto6.rb
@@ -1,12 +1,42 @@
+# frozen_string_literal: true
+
 class WebPages::Loto6 < WebPage
+  TABLE_CSS_SELECTOR = 'table.typeTK'
+  THEAD_CSS_SELECTOR = "#{TABLE_CSS_SELECTOR} thead"
+  TBODY_CSS_SELECTOR = "#{TABLE_CSS_SELECTOR} tbody"
+
   def url
     'https://www.mizuhobank.co.jp/retail/takarakuji/check/loto/loto6/index.html'
   end
 
   def times
-    times = driver.find_element(:css, 'table.typeTK thead tr th:nth-child(2)').text
-    m = Regexp.new('第([0-9]{1,})回').match(times)
-    raise NoMatchingPatternError if m.nil? || m[1].nil?
+    times = driver.find_element(:css, "#{THEAD_CSS_SELECTOR} tr th:nth-child(2)").text
+    match(times, '第([0-9]{1,})回')
+  end
+
+  def lottery_date
+    lottery_date = driver.find_element(:css, "#{TBODY_CSS_SELECTOR} tr:nth-child(1) td").text
+    Date.new(
+      match(lottery_date, '([0-9]{4})年[0-9]{1,2}月[0-9]{1,2}日').to_i,
+      match(lottery_date, '[0-9]{4}年([0-9]{1,2})月[0-9]{1,2}日').to_i,
+      match(lottery_date, '[0-9]{4}年[0-9]{1,2}月([0-9]{1,2})日').to_i,
+    )
+  end
+
+  def numbers
+    driver.find_elements(:css, "#{TBODY_CSS_SELECTOR} tr:nth-child(2) td strong").map { | element | element.text }
+  end
+
+  def bounus_number
+    bounus_number = driver.find_element(:css, "#{TBODY_CSS_SELECTOR} tr:nth-child(3) td strong").text
+    match(bounus_number, '\(([0-9]{2})\)')
+  end
+
+  private
+
+  def match(target, pattern)
+    m = Regexp.new(pattern).match(target)
+    return nil if m.nil? || m[1].nil?
 
     m[1]
   end
--- src/spec/models/web_pages/loto6_spec.rb
+++ src/spec/models/web_pages/loto6_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 module WebPages
   RSpec.describe Loto6, type: :model do
     let(:instance) { described_class.new }
     let(:url) { 'http://web:80/loto6/index.html' }
+    let(:expected_numbers) { ['02', '06', '20', '30', '31', '41'] }
 
     before do
       instance.get(url)
@@ -23,6 +25,24 @@ module WebPages
     describe '#times' do
       it '開催回を返却すること' do
         expect(instance.times).to eq '1763'
+      end
+    end
+
+    describe '#lottery_date' do
+      it '抽せん日を返却すること' do
+        expect(instance.lottery_date).to eq Date.new(2023, 2, 2)
+      end
+    end
+
+    describe '#numbers' do
+      it '本数字の配列を返却すること' do
+        expect(instance.numbers).to match_array expected_numbers
+      end
+    end
+
+    describe '#bounus_number' do
+      it 'ボーナス数字を返却すること' do
+        expect(instance.bounus_number).to eq '11'
       end
     end
   end
