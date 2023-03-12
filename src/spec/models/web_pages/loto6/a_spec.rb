d46916cceac838910fcba10218f5e99533d2a740
koba-masa <kobayashi_mshr@koba-masa.com>
2023-03-12 18:34:21 +0900

    refactor: 結果が1つしかない場合、複数ある場合のテストが実行できるように修正

--- src/spec/models/web_pages/loto6/a_spec.rb
+++ src/spec/models/web_pages/loto6/a_spec.rb
@@ -5,42 +5,44 @@ require 'rails_helper'
 module WebPages
   module Loto6
     RSpec.describe A do
-      let(:instance) { described_class.new(url) }
-      let(:url) { 'http://web/loto6/index.html' }
+      let(:instance_single) { described_class.new(url_single) }
+      let(:instance_multiple) { described_class.new(url_multiple) }
+      let(:url_single) { 'http://web/loto6/index_single.html' }
+      let(:url_multiple) { 'http://web/loto6/index_multiple.html' }
       let(:expected_numbers) { %w[02 06 20 30 31 41] }
 
       after do
-        instance.close
-        instance.quit
+        instance_single.close
+        instance_single.quit
       end
 
       describe '#initialize' do
         it 'インスタンス生成時にページの移動が完了していること' do
-          expect(instance.driver.current_url).to eq url
+          expect(instance_single.driver.current_url).to eq url_single
         end
       end
 
       describe '#times' do
         it '開催回を返却すること' do
-          expect(instance.times).to eq '1763'
+          expect(instance_single.times).to eq '1763'
         end
       end
 
       describe '#lottery_date' do
         it '抽せん日を返却すること' do
-          expect(instance.lottery_date).to eq Date.new(2023, 2, 2)
+          expect(instance_single.lottery_date).to eq Date.new(2023, 2, 2)
         end
       end
 
       describe '#numbers' do
         it '本数字の配列を返却すること' do
-          expect(instance.numbers).to match_array expected_numbers
+          expect(instance_single.numbers).to match_array expected_numbers
         end
       end
 
       describe '#bounus_number' do
         it 'ボーナス数字を返却すること' do
-          expect(instance.bounus_number).to eq '11'
+          expect(instance_single.bounus_number).to eq '11'
         end
       end
     end
