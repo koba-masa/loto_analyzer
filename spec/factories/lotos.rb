83f22cfffc8523e243f888f5d12bcd97f9ff59ed
koba-masa <kobayashi_mshr@koba-masa.com>
2023-05-27 20:31:29 +0900

    refactor: Loto用のファクトリーにsequenceを導入

--- spec/factories/lotos.rb
+++ spec/factories/lotos.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :loto do
     kind { Loto.kinds[:loto6] }
-    times { 10 }
-    lottery_date { Date.new(2023, 0o1, 0o1) }
+    sequence(:times, 1) { |n| n }
+    lottery_date { Date.new(2023, 1, 1) }
     sales_amount { 557_729_800 }
   end
 end
