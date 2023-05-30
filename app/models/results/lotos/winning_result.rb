bd419de14d2332fc0ad150e6c259b600e39f9072
koba-masa <kobayashi_mshr@koba-masa.com>
2023-05-30 21:45:26 +0900

    feat: 当選結果を出力するモデルを作成

---
+++ app/models/results/lotos/winning_result.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Results
+  module Lotos
+    class WinningResult
+      def initialize(kind)
+        @kind = kind
+      end
+
+      def create
+        processed_lotos = to_json(lotos)
+
+        FileUtils.mkdir_p(directory_path)
+
+        File.write(file_path, processed_lotos)
+      end
+
+      private
+
+      def lotos
+        @lotos ||= Loto.where(kind: Loto.kinds[@kind]).order(times: 'ASC').includes(:loto_numbers)
+      end
+
+      def to_json(lotos)
+        {
+          results: lotos.map do |loto|
+            {
+              times: loto.times,
+              lottery_date: loto.lottery_date.strftime('%Y/%m/%d'),
+              loto_numbers: loto.loto_numbers.unbouns.map(&:number),
+              bounus_loto_number: loto.loto_numbers.bouns[0].number,
+            }
+          end,
+        }.to_json
+      end
+
+      def directory_path
+        Settings.jobs.loto_result_creation_job.path
+      end
+
+      def file_path
+        "#{directory_path}/#{@kind}.json"
+      end
+    end
+  end
+end
